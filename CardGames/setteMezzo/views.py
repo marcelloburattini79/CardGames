2324f4ea996da7ae3c91d3c01f7999c374853476
marcelloburattini79 <burattinimarcello@yahoo.it>
2018-07-24 18:04:55 +0200

    24 luglio

--- CardGames/setteMezzo/views.py
+++ CardGames/setteMezzo/views.py
@@ -6,5 +6,13 @@ from django.shortcuts import render
 from .models import Carta
 
 def home(request):
+
     carte = Carta.objects.all()
+
     return render(request, 'home.html', {'carte': carte})
+
+def carta_dett(request, pk):
+
+    carta = Carta.objects.get(id=pk)
+
+    return render(request, 'carta_dett.html', {'carta': carta})
