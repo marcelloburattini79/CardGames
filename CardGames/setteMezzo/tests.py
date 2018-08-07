71248051cdf4e43f1db85f75331bdbc144266105
marcelloburattini79 <burattinimarcello@yahoo.it>
2018-08-08 00:48:37 +0200

    07 Agosto 18:00

--- CardGames/setteMezzo/tests.py
+++ CardGames/setteMezzo/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .views import home, carta_dett, new_topic
-from .models import Carta, seme
+from .models import Carta, seme, Topic, Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class HomeTests(TestCase):
@@ -45,17 +46,20 @@ class setteMezzoCarteTests(TestCase):
         view = resolve('/carta/1')
         self.assertEquals(view.func, carta_dett)
 
-    def test_carta_dett_view_contains_link_back_to_homepage(self):
+    def test_carta_dett_view_contains_navigation_link(self):
         carta_dett_url = reverse('carta', kwargs={'pk': 1})
-        response = self.client.get(carta_dett_url)
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
         homepage_url = reverse('home')
+        response = self.client.get(carta_dett_url)
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
 
 
 class NewTopicTests(TestCase):
     def setUp(self):
         seme_carta = seme.objects.create(value=2, colore="Spade")
         Carta.objects.create(value=1, tipo=seme_carta)
+        User.objects.create_user(username='john', email='john@doe.com', password='123')
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -76,4 +80,43 @@ class NewTopicTests(TestCase):
         carta_dett_url = reverse('carta', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(carta_dett_url))
+
+    def test_csrf(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': 'Test title',
+            'message': 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected behavior is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject': '',
+            'message': ''
+        }
+        response = self.client.post(url, data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
 
--- CardGames/setteMezzo/views.py
+++ CardGames/setteMezzo/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from .models import Carta, Topic, Post
 from django.http import Http404
+from .forms import NewTopicForm
 
 def home(request):
 
@@ -23,6 +24,30 @@ def carta_dett(request, pk):
     return render(request, 'carta_dett.html', {'carta': carta})
 
 def new_topic(request, pk):
+    carta = get_object_or_404(Carta, pk=pk)
+
+    user = User.objects.first()  # TODO: get the currently logged in user
+
+    if request.method == 'POST':
+
+        form = NewTopicForm(request.POST)
+
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.carta = carta
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('carta', pk=carta.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'carta': carta, 'form': form})
+
+def new_topic_vecchio(request, pk):
 
     try:
         carta = Carta.objects.get(id=pk)
